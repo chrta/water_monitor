fa3ec6d69d6bacbd2089b3b4103acd8c436fd0e3
Christian Taedcke <hacking@taedcke.com>
2016-12-04 17:58:31 +0100

    Add correct Kitto.root
    This ables the use in the umbrella project

--- config/config.exs
+++ config/config.exs
@@ -15,3 +15,5 @@ import_config "../apps/*/config/config.exs"
 #       level: :info,
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
+
+config :kitto, root: Path.dirname(__DIR__) <> "/apps/syr_dashboard", port: 4005
