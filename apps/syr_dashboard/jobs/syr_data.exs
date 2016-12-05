913ec843addee6a311199bb33dfa4a8fb2728212
Christian Taedcke <hacking@taedcke.com>
2016-12-05 21:59:52 +0100

    Remove unused code and log messages in syr_data job

--- apps/syr_dashboard/jobs/syr_data.exs
+++ apps/syr_dashboard/jobs/syr_data.exs
@@ -1,9 +1,6 @@
 use Kitto.Job.DSL
 
 defmodule Kitto.Jobs.SyrDeviceInfo do
-  use GenEvent
-
-  require Logger
 
   def stream(id, fun) do
     Task.start_link fn ->
@@ -24,31 +21,7 @@ defmodule Kitto.Jobs.SyrDeviceInfo do
       end
     end
   end
-
-  # Callbacks
-
-  def handle_event({:state, x}, messages) do
-    Logger.warn "New state #{inspect(x)}"
-    {:ok, [x | messages]}
-  end
-
-   def handle_event({:port_info, x}, messages) do
-     Logger.warn "New port_info #{inspect(x)}"    
-     {:ok, [x | messages]}
-   end
-
-   def handle_event({:device_info, x}, messages) do
-     Logger.warn "New device_info #{inspect(x)}"    
-     {:ok, [x | messages]}
-   end
-
-  def handle_call(:messages, messages) do
-    {:ok, Enum.reverse(messages), []}
-  end
-  
 end
-
-GenEvent.add_handler(:syr_event_manager, Kitto.Jobs.SyrDeviceInfo, [])
 
 job :water_pressure_bar, do: Kitto.Jobs.SyrDeviceInfo.stream(:prs, &(broadcast!(:water_pressure_bar, &1)))
 
